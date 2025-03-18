2be2a9359dbc8f967afb04e85f2ae37173cca92b
Kshitiz-Lall <kshitiz.lall@genzeon.com>
2025-03-19 01:49:15 +0530

    DB Connection Added

---
+++ backend/src/middleware/authMiddleware.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
+
+export interface AuthRequest extends Request {
+  userId?: string;
+}
+
+const authMiddleware = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authHeader = req.header("Authorization");
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: No token provided" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized: Invalid token" });
+  }
+};
+
+export default authMiddleware;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import formatRoutes from "./formatRoutes";
 import imageRoutes from "./imageRoutes";
 import apiRequestRoutes from "./apiRequestRoutes";
+import authRoutes from "./authRoutes";
 
 const router = express.Router();
 
 router.use("/format", formatRoutes);
 router.use("/image", imageRoutes);
 router.use("/request-tester", apiRequestRoutes);
+router.use("/auth", authRoutes);
 
 export default router;
